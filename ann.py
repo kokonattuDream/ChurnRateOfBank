6b482cee618fe1709fef945693da8c8bd5df3566
kokonattuDream <32606269+kokonattuDream@users.noreply.github.com>
2018-01-10 14:44:06 -0400

    Turning the ANN

--- ann.py
+++ ann.py
@@ -71,7 +71,6 @@ cm = confusion_matrix(y_test, y_pred)
 
 """""
 Case Study
-
 Country: France
 Credit Score: 600
 Gender: Male
@@ -105,3 +104,32 @@ accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, c
 
 mean = accuracies.mean()
 variance = accuracies.std()
+
+
+# Tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    
+    return classifier
+
+
+classifier = KerasClassifier(build_fn = build_classifier)
+
+parameters = {'batch_size': [25, 32], 'epochs': [100, 500], 'optimizer': ['adam', 'rmsprop']}
+
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
